335b42e8ee5bd14b00ce01782fd4638d860b3a33
OrapelengN <portiaoran@gmail.com>
2024-12-23 10:36:42 -0800

    Fixed memory leaks and coding style

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,6 +4,9 @@
 /**
  * binary_tree_delete - Deletes an entire binary tree
  * @tree: Pointer to the root node of the tree to delete
+ * 
+ * This function recursively deletes all nodes in the binary tree,
+ * starting from the root and moving downwards to the leaves.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
